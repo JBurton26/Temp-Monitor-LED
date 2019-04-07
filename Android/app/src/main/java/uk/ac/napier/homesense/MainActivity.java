9c07b267ddf8bb16d8cbdf18bfc461fcf7c2cc64
JBurton26 <40278490@users.noreply.github.com>
2019-04-07 15:35:43 +0100

    MQTT Subscription added

--- Android/app/src/main/java/uk/ac/napier/homesense/MainActivity.java
+++ Android/app/src/main/java/uk/ac/napier/homesense/MainActivity.java
@@ -1,14 +1,46 @@
 package uk.ac.napier.homesense;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import org.eclipse.paho.client.mqttv3.*;
+
+import helpers.MQTTHelper;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    MQTTHelper mqttHelper;
+    TextView dataReceived;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dataReceived = findViewById(R.id.dataReceived);
+        startMqtt();
+    }
+    private void startMqtt() {
+        mqttHelper = new MQTTHelper(getApplicationContext());
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("Debug", mqttMessage.toString());
+                dataReceived.setText(mqttMessage.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
     }
 }
